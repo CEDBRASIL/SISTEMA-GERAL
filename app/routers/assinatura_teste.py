05403d2d14befc64228a383349a2edbfd38757c5
CEDBRASIL <cedbrasil@outlook.com>
2025-06-02 00:44:50 -0300

    dadada

--- app/routers/assinatura_teste.py
+++ app/routers/assinatura_teste.py
@@ -1,1 +1,10 @@
-# TODO
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/ping")
+def ping():
+    """
+    Rota de teste simples para validar se o módulo assinatura_teste está funcionando.
+    """
+    return {"mensagem": "Assinatura Teste OK"}
